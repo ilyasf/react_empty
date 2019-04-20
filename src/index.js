2f10d1d387d2356b728da0036ae62bf5368dc00c
ilias <ilyasf@yandex.ru>
2019-04-20 06:45:31 +0300

    Material UI

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React APP</h1>
-      <h2>Just start doing frontend easy!</h2>
-    </div>
+    <React.Fragment>
+        <CssBaseline />
+        <div className="App">
+            <h1>React APP</h1>
+            <h2>Just start doing frontend easy!</h2>
+        </div>
+    </React.Fragment>
   );
 }
 
